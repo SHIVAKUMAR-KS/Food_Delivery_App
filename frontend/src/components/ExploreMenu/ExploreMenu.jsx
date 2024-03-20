72644e7627ff3056c5fde22d8d031e6349f9dc23
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2024-03-20 22:49:46 +0530

    Ended explore menu with hovering on item

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
 import './ExploreMenu.css'
-import {menu_list} from '../../assets/assets'
-const ExploreMenu = () => {
+import { menu_list } from '../../assets/assets'
+
+const ExploreMenu = ({ category, setcategory }) => {
   return (
     <div className='explore-menu' id='explore-menu'>
-        <h1>Explore Our Menu</h1>
-        <p>Choose from a diverse menu featuring a delectable array</p>
-        <div className="explore-menu-list">
-            {menu_list.map((item,index)=>{
-                return (
-                    <div key={index}className="explore-menu-list-item">
-                        <img src={item.menu_image} alt="" />
-                        <p>{item.menu_name}</p>
-                    </div>
-                )
-            })}
-        </div>
-        <hr />
+      <h1>Explore Our Menu</h1>
+      <p>Choose from a diverse menu featuring a delectable array</p>
+      <div className="explore-menu-list">
+        {menu_list.map((item, index) => {
+          return (
+            <div onClick={() => setcategory(prev => prev === item.menu_name ? "All" : item.menu_name)} key={index} className="explore-menu-list-item">
+              <img className={category === item.menu_name ? "active" : ""} src={item.menu_image} alt="" />
+              <p>{item.menu_name}</p>
+            </div>
+          )
+        })}
+      </div>
+      <hr />
     </div>
   )
 }
